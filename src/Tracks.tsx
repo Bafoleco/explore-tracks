c89b80206d886ab1284b17d3984d7eb240aa18b0
Bay Foley-Cox <bayfoleycox@gmail.com>
2022-06-15 13:25:17 -0400

    more tracks

--- src/Tracks.tsx
+++ src/Tracks.tsx
@@ -11,6 +11,11 @@ import { systems } from './tracks/systems';
 import { hci } from './tracks/hci';
 import { security } from './tracks/security';
 import { theory } from './tracks/theory';
+import { realworld } from './tracks/realworld';
+import { ai } from './tracks/ai';
+import { biocomputation } from './tracks/biocomputation';
+import { information } from './tracks/information';
+import { softwaretheory } from './tracks/softwaretheory';
 
 const Tracks = () => {
 
@@ -29,12 +34,22 @@ const Tracks = () => {
               <Tab label="HCI" value="2" />
               <Tab label="Security" value="3" />
               <Tab label="Theory" value="4" />
+              <Tab label="Realworld Computing" value="5" />
+              <Tab label="AI" value="6" />
+              <Tab label="Biocomputation" value="7" />
+              <Tab label="Information" value="8" />
+              <Tab label="Software Theory" value="9" />
             </TabList>
           </Box>
           <TabPanel value="1"> <Track trackInfo={systems}/> </TabPanel>
           <TabPanel value="2"> <Track trackInfo={hci}/> </TabPanel>
           <TabPanel value="3"> <Track trackInfo={security}/> </TabPanel>
           <TabPanel value="4"> <Track trackInfo={theory}/> </TabPanel>
+          <TabPanel value="5"> <Track trackInfo={realworld}/> </TabPanel>
+          <TabPanel value="6"> <Track trackInfo={ai}/> </TabPanel>
+          <TabPanel value="7"> <Track trackInfo={biocomputation}/> </TabPanel>
+          <TabPanel value="8"> <Track trackInfo={information}/> </TabPanel>
+          <TabPanel value="9"> <Track trackInfo={softwaretheory}/> </TabPanel>
         </TabContext>
       </Box>
     );
